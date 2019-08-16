ad6bb96809f1cef4699827a953732f02a4230453
Armando Roman <armandoroman1016@gmail.com>
2019-08-16 08:27:39 -0700

    basic reducer set up

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from 'react-redux';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
+        <div>{this.props.title}</div>
         <div>Have fun!</div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    title : state.title,
+    smurfs : state.smurfs
+  }
+}
+
+export default connect(mapStateToProps,{})(App)
---
+++ smurfs/src/reducers/smurfsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+
+    smurfs : [],
+    title : 'HELLLLLOOOOO'
+
+}
+
+export const reducer = ( state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
